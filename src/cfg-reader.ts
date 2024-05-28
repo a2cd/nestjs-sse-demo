617d4170b8e6b49e1104e5943c7afe4004bbc89a
Kai <ca2pa23187@gmail.com>
2024-05-28 15:09:01 +0800

    refactor: 读取配置加try-catch

--- src/cfg-reader.ts
+++ src/cfg-reader.ts
@@ -6,16 +6,36 @@ import * as _ from 'loadsh';
 function loadCfg(): any {
   const projectRoot = process.cwd();
   console.log('projectRoot=' + projectRoot);
+  const cfgPath = path.join(projectRoot, 'cfg');
   // 加载主配置
-  const mainPath = path.join(projectRoot, 'cfg', 'main.toml');
-  const mainFileContent = readFile(mainPath);
-  const mainCfg: any = parse(mainFileContent);
+  const mainFilePath = path.join(cfgPath, 'main.toml');
+  let mainFileContent;
+  try {
+    mainFileContent = readFile(mainFilePath);
+  } catch (e) {
+    throw new Error(`main.toml 文件异常: ${e}`);
+  }
+  let mainCfg: any;
+  try {
+    mainCfg = parse(mainFileContent);
+  } catch (e) {
+    throw new Error(`解析main.toml 文件异常: ${e}`);
+  }
   // 加载指定配置
-  const specificFileContent = readFile(
-    path.join(projectRoot, 'cfg', `${mainCfg.server.use}.toml`),
-  );
-  const specificCfg: any = parse(specificFileContent);
-  console.log(specificCfg);
+  const specificFilePath = path.join(cfgPath, `${mainCfg.server.use}.toml`);
+  let specificFileContent;
+  try {
+    specificFileContent = readFile(specificFilePath);
+  } catch (e) {
+    throw new Error(`${mainCfg.server.use}.toml 文件异常: ${e}`);
+  }
+  let specificCfg: any;
+  try {
+    specificCfg = parse(specificFileContent);
+  } catch (e) {
+    throw new Error(`解析${mainCfg.server.use}.toml 文件异常: ${e}`);
+  }
+  // 合并主配置与指定配置
   return _.merge(mainCfg, specificCfg);
 }
 
